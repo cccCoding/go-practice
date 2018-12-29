ca2aec50b3e1dd9aa9d48478a453453e25219843
cccCoding <826674632@qq.com>
2018-12-29 07:15:56 -0800

    slice

--- day4/example7/main/main.go
+++ day4/example7/main/main.go
@@ -6,6 +6,32 @@ import "fmt"
 func main() {
 	testSlice()
 	modifyString()
+	testSlice2()
+	testCopy()
+}
+
+func testCopy() {
+	fmt.Println("testcopy-----------------------")
+	var a = []int{1,2,3,4,5,6}
+	b := make([]int, 1)
+	copy(b, a)			//拷贝第一个元素过来，不追加
+	fmt.Println(b)
+}
+
+func testSlice2() {
+	fmt.Println("test2-----------------------")
+	var a = [5]int{1,2,3,4,5}
+	s := a[1:]
+	fmt.Printf("s=%p a[1]=%p\n", s, &a[1])	//相同//通过数组得到的切片地址为数组对应index元素的地址
+	fmt.Printf("len=%d,cap=%d\n", len(s), cap(s))
+
+	s[1] = 100
+	fmt.Printf("s=%p a[1]=%p\n", s, &a[1])	//相同，切片还是原来的切片
+	fmt.Printf("len=%d,cap=%d\n", len(s), cap(s))
+	s = append(s, 10)
+	fmt.Printf("s=%p a[1]=%p\n", s, &a[1])	//不同，重新申请了内存，切片内部指向另一个数组
+	fmt.Printf("len=%d,cap=%d\n", len(s), cap(s))		//指向的另一个数组长度翻倍了
+	fmt.Println(s)
 }
 
 func testSlice() {
